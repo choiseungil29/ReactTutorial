3c1760a6ce10545338fdab17008da813a5992452
clogic <choiseungil29@gmail.com>
2016-11-21 19:04:23 +0900

    first init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+        let name = 'clogic';
+		return (
+            <div>
+			 <h1>Hello React {name}</h1>
+            </div>
+		);
+	}
+}
+
+export default App;
